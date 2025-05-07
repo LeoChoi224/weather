4484e18e75876e69fc64ba7b208f5203eab46ec2
HongmookChoi <leochoi224@gmail.com>
2025-05-07 18:34:39 +0900

    chap.7

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,11 +2,13 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
 public class WeatherApplication {
 
     public static void main(String[] args) {
@@ -14,3 +16,8 @@ public class WeatherApplication {
     }
 
 }
+
+
+
+
+
